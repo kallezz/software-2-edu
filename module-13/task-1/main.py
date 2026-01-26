3e45c095c13c7c149a6f5294acb132e234a84bc3
kallezz <kalle.melender@hotmail.com>
2026-01-26 11:24:13 +0200

    add module 13

---
+++ module-13/task-1/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/alkuluku/<int:number>")
+def alkuluku(number):
+    if number <= 1:
+        is_prime = False
+    else:
+        is_prime = True
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                is_prime = False
+                break
+
+    if is_prime:
+        return {"number": number, "isPrime": True}
+    else:
+        return {"number": number, "isPrime": False}
